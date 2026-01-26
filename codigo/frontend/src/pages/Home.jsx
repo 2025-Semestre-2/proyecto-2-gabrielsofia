409d3f1c7a472ec0bc930a9662051c909d01db40
Gabriel Garro <g.garro.1@estudiantec.cr>
2026-01-26 12:07:02 -0600

    Boton en el main para cambiar de seccion

--- codigo/frontend/src/pages/Home.jsx
+++ codigo/frontend/src/pages/Home.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate(); // ✅ aquí adentro
+
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Buscador de Hoteles</h1>
@@ -8,7 +12,10 @@ export default function Home() {
         placeholder="Ubicación"
       />
 
-      <button className="bg-blue-600 text-white px-4 py-2">
+      <button
+        onClick={() => navigate("/admin/hoteles")}
+        className="bg-blue-600 text-white px-4 py-2 mr-2"
+      >
         Buscar
       </button>
     </div>
