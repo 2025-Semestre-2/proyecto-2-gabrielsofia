62a57e590ec1650b484eaba206396d2a081280d8
dofi2323 <sofiae111105@gmail.com>
2026-01-26 20:12:29 -0600

    algo

---
+++ codigo/sistema-hospedaje/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <header className="bg-white text-black px-6 py-4 shadow">
+      <h1 className="font-bold text-xl">
+        Navbar OK
+      </h1>
+    </header>
+  );
+}
---
+++ codigo/sistema-hospedaje/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../layouts/MainLayout";
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <h1 className="text-3xl font-bold text-red-400">
+        HOME FUNCIONA 🔥
+      </h1>
+    </MainLayout>
+  );
+}
